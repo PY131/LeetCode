b0e126bd4f7101b8448ab75f19827a34018574a4
PY131 <289683309@qq.com>
2017-01-06 20:29:15 +0800

    add the first java code for 461.

---
+++ algorithms/java/461_hamming_distance/hamming_distance_1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class hamming_distance {
+    
+	public int hammingDistance(int x, int y) {
+        int ham_dis;
+        int z = x ^ y; 
+        for(ham_dis=0; z!=0; z=z>>1){
+            ham_dis += z & 1;
+        }
+        return ham_dis;
+    }
+	
+	public static void main(String[] args) {
+		int x1, x2;
+		hamming_distance hd1 = new hamming_distance();
+		Scanner sc1 = new Scanner(System.in);
+		
+		System.out.println("input two integers:");
+		x1 = sc1.nextInt();
+		x2 = sc1.nextInt();
+		System.out.printf("hamming distance is: %d", hd1.hammingDistance(x1, x2));
+	}
+
+}
