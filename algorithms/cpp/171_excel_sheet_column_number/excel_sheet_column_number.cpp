f4af95dde73c5ae66700b13447c5a52be02cffe7
P. Yuan <289683309@qq.com>
2017-01-19 15:25:22 +0800

    Update excel_sheet_column_number.cpp

--- algorithms/cpp/171_excel_sheet_column_number/excel_sheet_column_number.cpp
+++ algorithms/cpp/171_excel_sheet_column_number/excel_sheet_column_number.cpp
@@ -15,28 +15,28 @@ using namespace std;
 class Solution1 {
 public:
     int titleToNumber(string s) {
-		int n = 0;
+	int n = 0;
 
-		if(s == "") return 0;
+	if(s == "") return 0;
 
-		n += ( *(s.end() - 1) - 'A') + 1;
-		s.pop_back();  //remove the last char
-		return n += titleToNumber(s) * 26;
+	n += ( *(s.end() - 1) - 'A') + 1;
+	s.pop_back();  //remove the last char
+	return n += titleToNumber(s) * 26;
     }
 };
 
 /**********************************************************
- * the 2nd solution using while loop object to 168's Solution2
+ * the 2nd solution using for loop object to 168's Solution2
  ***********************************************************/
 class Solution2 {
 public:
     int titleToNumber(string s) {
-		int n = 0;
+	int n = 0;
 
-		for(unsigned int i = 0; i < s.size(); i++){
-			n = n * 26 + (s[i] - 'A') + 1;
-		}
-		return n;
+	for(unsigned int i = 0; i < s.size(); i++){
+		n = n * 26 + (s[i] - 'A') + 1;
+	}
+	return n;
     }
 };
 
